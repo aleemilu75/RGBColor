9692082d26b148b67054fa836605e9cdd48ced80
Ranjan <Ranjan-kumar.jena@deutschebahn.com>
2017-09-21 12:30:58 +0530

    initial commit

---
+++ RGBColor/RGBColor.h
@@ -0,0 +1,18 @@
+//
+//  RGBColor.h
+//  RGBColor
+//
+//  Created by niit on 9/21/17.
+//  Copyright © 2017 niit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for RGBColor.
+FOUNDATION_EXPORT double RGBColorVersionNumber;
+
+//! Project version string for RGBColor.
+FOUNDATION_EXPORT const unsigned char RGBColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGBColor/PublicHeader.h>
+
+
---
+++ RGBColor/RGBViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RGBViewController.swift
+//  RGBColor
+//
+//  Created by niit on 9/21/17.
+//  Copyright © 2017 niit. All rights reserved.
+//
+
+import UIKit
+
+public class RGBViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    public func printLogs() {
+        print("Hi their")
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
